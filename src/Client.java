070dd295ac809718c1b1156f4f9a959c2debfa78
DELL <DELL@DESKTOP-M6G193G>
2024-05-20 12:22:03 +0200

    Interface 2

--- src/Client.java
+++ src/Client.java
@@ -7,15 +7,18 @@ import java.util.Map;
 public class Client {
     private String nom;
      private String prenom;
-    private String type; // Acheteur, locataire, vendeur, bailleur
-    private Map<String, String> informations; // Informations supplémentaires sur le client
-    private List<String> historiqueInteractions;
+    private String adresse; // Informations supplémentaires sur le client
+     private String type; // Acheteur, locataire, vendeur, bailleur
 
-    public Client(String nom, String type) {
+    public Client(String nom, String prenom, String adresse, String type) {
         this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
         this.type = type;
-        this.informations = new HashMap<>();
-        this.historiqueInteractions = new ArrayList<>();
+    }
+
+    public String getAdresse() {
+        return adresse;
     }
 
     public String getNom() {
@@ -30,51 +33,27 @@ public class Client {
         return type;
     }
 
-    public Map<String, String> getInformations() {
-        return informations;
-    }
-
-    public List<String> getHistoriqueInteractions() {
-        return historiqueInteractions;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
     }
 
     public void setPrenom(String prenom) {
         this.prenom = prenom;
     }
 
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
     public void setType(String type) {
         this.type = type;
     }
+  
 
-    public void setInformations(Map<String, String> informations) {
-        this.informations = informations;
+    @Override
+    public String toString() {
+     String affichage = "le nom de client est "+getNom()+" de prenom "+getPrenom()+" et l'adresse "+getAdresse()+" de type "+getType();
+        return affichage;
     }
-
-    public void setHistoriqueInteractions(List<String> historiqueInteractions) {
-        this.historiqueInteractions = historiqueInteractions;
-    }
-
-    public void ajouterInformation(String cle, String valeur) {
-        informations.put(cle, valeur);
-    }
-
-    public void modifierInformation(String cle, String nouvelleValeur) {
-        if (informations.containsKey(cle)) {
-            informations.put(cle, nouvelleValeur);
-        } else {
-            System.out.println("Cette information n'existe pas pour ce client.");
-        }
-    }
-
-    public void supprimerInformation(String cle) {
-        informations.remove(cle);
-    }
-
-    public void ajouterInteraction(String interaction) {
-        historiqueInteractions.add(interaction);
-    } 
+    
 }
--- src/fenetre.java
+++ src/fenetre.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -26,28 +29,107 @@ public class fenetre extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        buttonGroup1 = new javax.swing.ButtonGroup();
+        jLabel2 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setBackground(new java.awt.Color(255, 204, 204));
-        jLabel1.setOpaque(true);
+        jLabel2.setBackground(new java.awt.Color(255, 204, 204));
+        jLabel2.setIcon(new javax.swing.ImageIcon("C:\\Users\\DELL\\Desktop\\cours s4\\POO\\projet\\JavaApplication6\\src\\image\\blding.png")); // NOI18N
+        jLabel2.setOpaque(true);
+
+        jButton2.setFont(new java.awt.Font("Tempus Sans ITC", 3, 36)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(0, 102, 102));
+        jButton2.setText("FERMER");
+        jButton2.setOpaque(false);
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jButton3.setFont(new java.awt.Font("Tempus Sans ITC", 3, 36)); // NOI18N
+        jButton3.setForeground(new java.awt.Color(0, 102, 102));
+        jButton3.setText("TRANSACTION");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        jButton4.setFont(new java.awt.Font("Urdu Typesetting", 2, 36)); // NOI18N
+        jButton4.setForeground(new java.awt.Color(0, 102, 102));
+        jButton4.setText("CLIENT");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
+        jButton5.setFont(new java.awt.Font("Tempus Sans ITC", 3, 36)); // NOI18N
+        jButton5.setForeground(new java.awt.Color(0, 102, 102));
+        jButton5.setText("BIENS");
+        jButton5.setOpaque(false);
+
+        jLabel1.setFont(new java.awt.Font("Verdana", 3, 48)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(0, 204, 51));
+        jLabel1.setText("IMMOCLE");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 542, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 515, Short.MAX_VALUE))
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 577, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 116, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton5, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 285, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(68, 68, 68))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 536, Short.MAX_VALUE)
+            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(29, 29, 29))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // open clients
+        clients cl = new clients () ;
+        cl.show();  // display clients here
+        dispose(); //close current frame fenetre after open clients
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+      int choix = JOptionPane.showConfirmDialog(this,"Etes-vous sur de vouloir sortir?", "Confirmation", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+      if(choix==0)System.exit(0);
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -85,6 +167,12 @@ public class fenetre extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
 }
